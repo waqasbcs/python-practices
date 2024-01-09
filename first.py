e43dc21ad93afa5db58c82d4be62dce3c9b928ce
waqasbcs <waqasaliy2945@gmail.com>
2024-01-09 12:20:00 +0500

    started python

---
+++ first.py
@@ -0,0 +1,15 @@
+name = "waqas"
+age = 12.03
+age = 12
+is_adult = False
+print (name)
+print (age)
+print (is_adult)
+
+
+first_name = "waqas"
+last_name = "khan"
+age = 24
+address = "soran"
+mob = "03441949399"
+print (first_name, last_name, age, address, mob)
