705e8635fd4b603d4a0aa040bb8bea989dcaf5d5
waqasbcs <waqasaliy2945@gmail.com>
2024-02-13 17:41:35 +0500

    password.py

--- password.py
+++ password.py
@@ -5,6 +5,7 @@ def generate_password(length=12):
     # Define characters to be used in the password
     characters = string.ascii_letters + string.digits + string.punctuation
 
+
     # Generate the password
     password = ''.join(random.choice(characters) for i in range(length))
     
